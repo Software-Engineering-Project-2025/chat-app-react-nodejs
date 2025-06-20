74a81fc770819999202a39abc25b9a7d090f8a41
Ayhan Taha Pastutmaz <113910977+ayhan219@users.noreply.github.com>
2025-06-20 21:05:18 +0300

    Update App.js

--- public/src/App.js
+++ public/src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AutoLogout from "./components/AutoLogout"; // import the new component
 import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
 export default function App() {
   return (
     <BrowserRouter>
-      <AutoLogout>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/setAvatar" element={<SetAvatar />} />
-          <Route path="/" element={<Chat />} />
-        </Routes>
-      </AutoLogout>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
     </BrowserRouter>
   );
 }
