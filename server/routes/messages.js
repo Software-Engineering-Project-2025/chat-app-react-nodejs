b2fb2b1b49f7d1a7c70c5cd26307d9471703739d
VAGIF NABIEV <130098547+Vagif-Nabiev@users.noreply.github.com>
2025-06-22 21:26:02 +0300

    added delete message route

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,8 +1,9 @@
-const { addMessage, getMessages,editMessage } = require("../controllers/messageController");
+const { addMessage, getMessages,editMessage, deleteMessage } = require("../controllers/messageController");
 const router = require("express").Router();
 
 router.post("/addmsg/", addMessage);
 router.post("/getmsg/", getMessages);
 router.put("/editmsg/",editMessage)
+router.delete("/deletemsg/", deleteMessage);
 
 module.exports = router;
